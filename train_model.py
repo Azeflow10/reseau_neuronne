e2dfb0604b5bccbc3ade40ab0ace124fcca37a8b
Azeflow <88823281+Azeflow10@users.noreply.github.com>
2025-10-17 12:24:02 +0200

    V1.0

--- train_model.py
+++ train_model.py
@@ -1,16 +1,21 @@
 import tensorflow as tf
 
 def train_model(model, x_train, y_train):
+
+    #Compilation du modele avec optimiseur adam et donction de perte(classification multiclasses),accuracy comme metrique
     model.compile(
         optimizer='adam',
         loss='sparse_categorical_crossentropy',
         metrics=['accuracy']
     )
+    #Callbacks pour sauvegarder le meilleur modele et arret precoce
 
     callbacks = [
         tf.keras.callbacks.ModelCheckpoint("best_model.h5", save_best_only=True, monitor="val_accuracy"),
         tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, restore_best_weights=True)
     ]
+
+    #*
 
     history = model.fit(
         x_train, y_train,
